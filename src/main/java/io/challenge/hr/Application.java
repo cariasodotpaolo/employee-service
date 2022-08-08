9a110602f1757e84608db3f8318771e3ec65e51b
Michael Paolo Cariaso <mpcariaso@gmail.com>
2022-08-08 17:12:34 +0800

    Project structure, initial commit.

---
+++ src/main/java/io/challenge/hr/Application.java
@@ -0,0 +1,16 @@
+package io.challenge.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Paolo Cariaso
+ * @date 8/8/2022 3:36 PM
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
